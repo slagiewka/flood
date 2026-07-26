d5ea09624cb8f770afc73444376157003ef6952e
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2026-07-26 17:16:26 +0200

    build: use replace rolldown with vite

--- vite.server.config.ts
+++ vite.server.config.ts
@@ -1,7 +1,36 @@
+import path from 'node:path';
+
 import {defineConfig} from 'vite';
 
-export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
+import {buildPaths} from './shared/config/buildPaths.mjs';
+export default defineConfig(({command}) => {
+  if (command === 'serve') {
+    return {
+      resolve: {
+        tsconfigPaths: true,
+      },
+    };
+  }
+
+  return {
+    ssr: {
+      noExternal: true,
+    },
+    build: {
+      emptyOutDir: false,
+      minify: false,
+      outDir: path.resolve(buildPaths.appSrc, 'dist'),
+      rolldownOptions: {
+        external: ['geoip-country'],
+        output: {
+          codeSplitting: false,
+          entryFileNames: 'index.js',
+          format: 'esm',
+        },
+      },
+      sourcemap: 'inline',
+      ssr: path.resolve(buildPaths.appSrc, 'server/bin/start.ts'),
+      target: 'node22',
+    },
+  };
 });
